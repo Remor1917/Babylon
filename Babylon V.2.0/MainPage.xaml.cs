38686bb7ff2014a18d6b1818ca05b1441c00d081
lekto <lekto@DESKTOP-CHDKO7I>
2024-04-14 00:54:35 +0600

    Добавьте файлы проекта.

---
+++ Babylon V.2.0/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Babylon_V._2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UP_Button(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void Reg_Baton(object sender, RoutedEventArgs e)
+        {
+            var reg = new Reg_Window();
+
+            reg.Show();
+        }
+
+        private void UnReg_Baton(object sender, RoutedEventArgs e)
+        {
+            var unreg = new Unreg_Window();
+
+            unreg.Show();
+        }
+    }
+}
---
+++ Babylon V.2.0/Unreg_Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Babylon_V._2._0
+{
+    /// <summary>
+    /// Логика взаимодействия для Unreg_Window.xaml
+    /// </summary>
+    public partial class Unreg_Window : Window
+    {
+        public Unreg_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
